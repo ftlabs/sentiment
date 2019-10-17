a9498acb79215ba5bdf96c04ad07568a79f3830c
Jamie Brown <jamiebrown201@hotmail.com>
2019-10-17 11:44:33 +0100

    message

--- handler.js
+++ handler.js
@@ -5,11 +5,12 @@ module.exports.main = async event => {
   const result = await fetchContent.getArticle(
     "5200d5fe-efda-11e9-bfa4-b25f11f42901"
   );
+  const articleContent = result.bodyXML;
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: result,
+        message: articleContent,
         input: event
       },
       null,
