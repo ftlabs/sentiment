1c03d6611428c16a57c279f786036915c72bb91f
Jamie Brown <jamiebrown201@hotmail.com>
2019-10-22 12:18:49 +0100

    adds ibm

--- handler.js
+++ handler.js
@@ -1,12 +1,13 @@
 "use strict";
 const fetchContent = require("./lib/fetchContent");
 const aws = require("./services/aws");
+const ibm = require("./services/ibm");
 
 module.exports.main = async event => {
   const uuid = event.pathParameters.uuid;
   const result = await fetchContent.getArticle(uuid);
   const articleContent = result.bodyXML;
-  const sentimentResult = await aws.getSentiment(articleContent);
+  const sentimentResult = await ibm.getSentiment(articleContent);
   return {
     statusCode: 200,
     body: JSON.stringify(
