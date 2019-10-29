529dcf7dfe7f5d7aafd8e075b9a1fc67814f2584
Jamie Brown <jamiebrown201@hotmail.com>
2019-10-29 13:00:53 +0000

    integrate

--- handler.js
+++ handler.js
@@ -13,25 +13,29 @@ module.exports.main = async event => {
   const title = result.title;
   const standfirst = result.standfirst;
 
-  // const sentimentResult = await Promise.all([
-  //   ibm.getSentiment({
-  //     articleContent,
-  //     title,
-  //     standfirst
-  //   }),
-  //   aws.getSentiment({
-  //     articleContent,
-  //     title,
-  //     standfirst
-  //   })
-  // ]);
+  const sentimentResult = await Promise.all([
+    ibm.getSentiment({
+      articleContent,
+      title,
+      standfirst
+    }),
+    aws.getSentiment({
+      articleContent,
+      title,
+      standfirst
+    }),
+    meaningCloud.getSentiment({
+      articleContent,
+      title,
+      standfirst
+    })
+  ]);
 
-  // const formattedResult = { ibm: sentimentResult[0], aws: sentimentResult[1] };
-  const formattedResult = await meaningCloud.getSentiment({
-    articleContent,
-    title,
-    standfirst
-  });
+  const formattedResult = {
+    ibm: sentimentResult[0],
+    aws: sentimentResult[1],
+    meaningCloud: sentimentResult[2]
+  };
 
   return {
     statusCode: 200,
