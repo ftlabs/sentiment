e2bbc879f2eaebd587cd0f6f47a16cb204093607
Jamie Brown <jamiebrown201@hotmail.com>
2019-11-21 15:00:35 +0000

    integrate fetch article

--- backend/aggregator.js
+++ backend/aggregator.js
@@ -1,7 +1,16 @@
 "use strict";
+const fetchArticles = require("./lib/fetchArticles");
 
 module.exports.main = async event => {
   try {
+    const result = await fetchArticles.search("brexit");
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result),
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      }
+    };
   } catch (err) {
     console.error(err);
     return {
