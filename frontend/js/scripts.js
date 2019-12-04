594285292a04b9f480243c43e242b98a5342282b
Jamie Brown <jamiebrown201@hotmail.com>
2019-12-04 14:29:56 +0000

    working matched sentences

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -16,8 +16,9 @@ async function submitForm(event) {
   let url =
     "https://umwi7bvika.execute-api.us-east-1.amazonaws.com/dev/ftlabs-sentiment/" +
     uuid;
-
+  let providersSelected;
   if (inputs.length !== 0) {
+    providersSelected = inputs.length;
     let providers = [];
     inputs.forEach(input => {
       if (input.checked) {
@@ -25,6 +26,8 @@ async function submitForm(event) {
       }
     });
     url = url + "?providers=" + providers.join(",");
+  } else {
+    providersSelected = 3;
   }
 
   let result = await fetch(url, {
@@ -50,12 +53,197 @@ async function submitForm(event) {
       sentenceData[provider] = result[provider].sentences;
     }
   });
-  sentenceFormatter(sentenceData);
-  sentenceFormatterGroups(sentenceData);
+  const matchedSentenceData = sentenceFormatter(sentenceData);
+  sentenceFormatterGroups(matchedSentenceData, providersSelected);
 
   const resultContainer = document.querySelector(".result");
   resultContainer.classList.remove("hidden");
   loading.classList.add("hidden");
+}
+
+function sentenceFormatterGroups(matchedSentenceData, providersSelected) {
+  const onlyPairableSentences = matchedSentenceData.filter(
+    sentenceData => sentenceData.length === providersSelected
+  );
+  const standardisedSentenceData = onlyPairableSentences.map(sentenceData =>
+    getStandardisedSentenceData(sentenceData)
+  );
+  const groupedStandardisedSentenceData = groupStandardisedSentenceData(
+    standardisedSentenceData
+  );
+  // console.log(groupedStandardisedSentenceData);
+  displaySentenceData(groupedStandardisedSentenceData);
+}
+
+function displaySentenceData(groupedStandardisedSentenceData) {
+  groupedStandardisedSentenceData.forEach(sentenceData => {
+    console.log(sentenceData);
+    const selector =
+      sentenceData.length === 3
+        ? ".grouped-three-container"
+        : ".grouped-two-container";
+    sentenceData.matches.forEach(match => {
+      const sentencesContainer = document.querySelector(selector);
+      andSentenceElement(match, sentencesContainer);
+    });
+  });
+}
+
+function groupStandardisedSentenceData(standardisedSentenceData) {
+  let threeMatches = [];
+  let twoMatches = [];
+
+  standardisedSentenceData.forEach(sentenceData => {
+    let positive = 0;
+    let negative = 0;
+    let neutral = 0;
+    sentenceData.forEach(providerSentenceData => {
+      providerSentenceData.standardisedSentiment.forEach(sentiment => {
+        if (sentiment === "POSITIVE") {
+          positive = positive + 1;
+        } else if (sentiment === "NEUTRAL") {
+          neutral = neutral + 1;
+        } else if (sentiment === "NEGATIVE") {
+          negative = negative + 1;
+        }
+      });
+    });
+
+    if (positive === 2) {
+      twoMatches = [
+        ...twoMatches,
+        sentenceData.map(providerData => ({
+          ...providerData,
+          matchedData: "POSTIVE"
+        }))
+      ];
+    }
+    if (negative === 2) {
+      twoMatches = [
+        ...twoMatches,
+        sentenceData.map(providerData => ({
+          ...providerData,
+          matchedData: "NEGATIVE"
+        }))
+      ];
+    }
+    if (neutral === 2) {
+      twoMatches = [
+        ...twoMatches,
+        sentenceData.map(providerData => ({
+          ...providerData,
+          matchedData: "NEUTRAL"
+        }))
+      ];
+    }
+    if (positive === 3) {
+      threeMatches = [
+        ...threeMatches,
+        sentenceData.map(providerData => ({
+          ...providerData,
+          matchedData: "POSTIVE"
+        }))
+      ];
+    }
+    if (negative === 3) {
+      threeMatches = [
+        ...threeMatches,
+        sentenceData.map(providerData => ({
+          ...providerData,
+          matchedData: "NEGATIVE"
+        }))
+      ];
+    }
+    if (neutral === 3) {
+      threeMatches = [
+        ...threeMatches,
+        sentenceData.map(providerData => ({
+          ...providerData,
+          matchedData: "NEUTRAL"
+        }))
+      ];
+    }
+  });
+  return [
+    { length: 3, matches: threeMatches },
+    { length: 2, matches: twoMatches }
+  ];
+}
+
+function getStandardisedSentenceData(sentenceData) {
+  const standardisedData = sentenceData.map(sentence => {
+    switch (sentence.provider) {
+      case "aws":
+        return { ...sentence, standardisedSentiment: [sentence.Sentiment] };
+      case "ibm":
+        const standardisedScores = ibmStandardised(sentence.tones);
+        return { ...sentence, standardisedSentiment: standardisedScores };
+      case "meaningCloud":
+        const standardisedScore = meaningCloudStandardised(sentence.score_tag);
+        if (standardisedScore) {
+          return { ...sentence, standardisedSentiment: [standardisedScore] };
+        } else {
+          return { ...sentence, standardisedSentiment: [] };
+        }
+      default:
+        return { ...sentence, standardisedSentiment: [] };
+    }
+  });
+  return standardisedData;
+}
+
+function ibmStandardised(tones) {
+  let sentimentArray = [];
+  tones.forEach(toneData => {
+    switch (toneData.tone_id) {
+      case "anger":
+        if (!sentimentArray.includes("NEGATIVE")) {
+          sentimentArray = [...sentimentArray, "NEGATIVE"];
+        }
+      case "fear":
+        if (!sentimentArray.includes("NEGATIVE")) {
+          sentimentArray = [...sentimentArray, "NEGATIVE"];
+        }
+      case "sadness":
+        if (!sentimentArray.includes("NEGATIVE")) {
+          sentimentArray = [...sentimentArray, "NEGATIVE"];
+        }
+      case "analytical":
+        if (!sentimentArray.includes("NEUTRAL")) {
+          sentimentArray = [...sentimentArray, "NEUTRAL"];
+        }
+      case "tentative":
+        if (!sentimentArray.includes("NEUTRAL")) {
+          sentimentArray = [...sentimentArray, "NEUTRAL"];
+        }
+      case "joy":
+        if (!sentimentArray.includes("POSITIVE")) {
+          sentimentArray = [...sentimentArray, "POSITIVE"];
+        }
+      case "confident":
+        if (!sentimentArray.includes("POSITIVE")) {
+          sentimentArray = [...sentimentArray, "POSITIVE"];
+        }
+      default:
+        break;
+    }
+  });
+  return sentimentArray;
+}
+
+function meaningCloudStandardised(score_tag) {
+  switch (score_tag) {
+    case "P+":
+      return "POSITIVE";
+    case "P":
+      return "POSITIVE";
+    case "NEU":
+      return "NEUTRAL";
+    case "N+":
+      return "NEGATIVE";
+    default:
+      break;
+  }
 }
 
 function addHeadings({ title, fullArticle, standfirst }) {
@@ -97,6 +285,7 @@ function sentenceFormatter(sentenceData) {
     const sentencesContainer = document.querySelector(".sentences-container");
     andSentenceElement(match, sentencesContainer);
   });
+  return matches;
 }
 
 function andSentenceElement(match, sentencesContainer) {
